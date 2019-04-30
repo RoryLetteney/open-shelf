36d9fecfb1cebdf7d45dea57ed6dcbc7bfcc80a7
RoryLetteney <rory.letteney@gmail.com>
2019-04-30 10:06:17 -0500

    Index.js serving homepage and styles working. Proof of life!!!!

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ const express = require('express'),
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('./public'));
+app.use(express.static('./public/styles'));
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
