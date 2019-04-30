dd320584e9843473250ee44696bcefbf137152b0
RoryLetteney <rory.letteney@gmail.com>
2019-04-30 10:35:57 -0500

    Book constructor created.

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
+
+// BOOK CONSTRUCTOR
+function Book(book) {
+  this.image = book.image || 'https://via.placeholder/50x100',
+  this.title = book.title || 'No title',
+  this.author = book.author || 'No author',
+  this.description = book.description || 'No description';
+}
