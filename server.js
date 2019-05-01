c91d612a99f7914373c75ee0d6f91fa8696f73c5
RoryLetteney <rory.letteney@gmail.com>
2019-05-01 17:44:01 -0500

    Reformatted save-book form to be shown by select-book button. Form starts with no value for bookshelf.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('./public/styles'));
+app.use(express.static('./public/js'));
 const client = new pg.Client(DATABASE_URL);
 client.connect();
 client.on('err', err => console.log(err));
@@ -70,7 +71,8 @@ function Book(book) {
   this.author = book.authors ? book.authors[0] : (book.author ? book.author : 'Author Unknown'),
   this.image_url = book.imageLinks ? 'https' + book.imageLinks.thumbnail.slice(4) : (book.image_url ? book.image_url : 'https://placehold.it/100X200'),
   this.description = book.description || 'Description Unknown',
-  this.isbn = book.volumeInfo ? `${book.volumeInfo.industryIdentifiers[0].type} ${book.volumeInfo.industryIdentifiers[0].identifier}` : (book.isbn ? book.isbn : 'ISBN Unknown');
+  this.isbn = book.volumeInfo ? `${book.volumeInfo.industryIdentifiers[0].type} ${book.volumeInfo.industryIdentifiers[0].identifier}` : (book.isbn ? book.isbn : 'ISBN Unknown'),
+  this.bookshelf = book.bookshelf || 'Bookshelf Unknown';
 }
 
 const fetchBooksFromDB = handler => {
@@ -101,7 +103,7 @@ Book.fetchBooksFromAPI = query => {
 
 Book.prototype.save = function() {
   const SQL = 'INSERT INTO books (title, author, image_url, description, isbn, bookshelf) VALUES($1,$2,$3,$4,$5,$6) RETURNING id';
-  const values = [...Object.values(this), 1];
+  const values = Object.values(this);
 
   return client.query(SQL, values);
 };
