8d1bf1f956f97cbefeacf951f5c3fa900e557c6c
RoryLetteney <rory.letteney@gmail.com>
2019-05-01 19:27:11 -0500

    Added click functionality to show or hide the nav-list

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,14 @@
 'use strict';
 
 $(document).ready(() => {
+
+  // SAVE STYLING FUNCTIONALITY
   $('.save-book') ? $('.save-book').hide() : '';
   $('.select-book') ? $('.select-book').click(function() {
     $(this).prev().show();
     $(this).hide();
   }) : '';
+
+  // NAV STYLING FUNCTIONALITY
+  $('#nav-icon').click(() => $('#nav-list').toggle('show'));
 });
